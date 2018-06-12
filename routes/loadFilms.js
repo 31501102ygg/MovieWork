635b89f08e570a3aac873482ab6c8554ed6ecf3c
31501102ygg <33078840+31501102ygg@users.noreply.github.com>
2018-06-12 14:55:33 +0800

    6.12
    登录、主页

---
+++ routes/loadFilms.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var URL = require('url');
+//加载mysql模块
+var mysql      = require('mysql');
+//创建连接
+var connection = mysql.createConnection({
+host     : 'localhost',
+user     : 'root',
+password : '123456',
+port     : "3306",
+database : 'web'
+});
+//执行创建连接 
+connection.connect();
+//SQL语句
+var  sql = 'SELECT * FROM films ';
+
+router.get('/', function(req, res, next) {
+    //解析请求参数
+
+    //查
+    connection.query(sql,function (err, result) {
+        if(err){
+          console.log('[SELECT ERROR] - ',err.message);
+          return;
+        }
+        
+        console.log(result);
+        //把搜索值输出
+       res.jsonp(result);
+    });
+});
+
+
+
+module.exports = router;
