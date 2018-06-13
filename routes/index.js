d2a8e7ec80861cba58b034a1c6df8b21609e99e3
ty-12138 <1643686024@qq.com>
2018-06-13 08:21:10 +0800

    account 0.2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile("C:/Users/de'l'l/Web2/web/public/signup.html");
+  res.sendFile("E:/Github/MovieWork/public/signup.html");
 });
 
 module.exports = router;
